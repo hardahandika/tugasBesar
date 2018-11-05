035d7a9fcaf5187b752363e04c3b85b48973fae1
arSurya <rahmanulquiorra@gmail.com>
2018-11-05 20:24:13 +0700

    versi 1.0.0

---
+++ app/src/main/java/com/abdur/android/galleryproject/MainActivity.java
@@ -0,0 +1,70 @@
+package com.abdur.android.galleryproject;
+
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gv;
+    ArrayList<File> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = imageReader(Environment.getExternalStorageDirectory());
+
+        gv = (GridView) findViewById(R.id.gridView);
+        gv.setAdapter(new GridAdapter());
+    }
+    class GridAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.single_grid,parent,false);
+            ImageView lv = (ImageView) convertView.findViewById(R.id.imageView);
+            lv.setImageURI(Uri.parse(getItem(position).toString()));
+            return convertView;
+        }
+    }
+
+    ArrayList<File> imageReader(File root){
+        ArrayList<File> a = new ArrayList<>();
+        File[] files = root.listFiles();
+        for(int i = 0 ; i<files.length; i++){
+            if(files[i].isDirectory()){
+                a.addAll(imageReader(files[i]));
+            }
+            else {
+                if(files[i].getName().endsWith(".jpg")){
+                    a.add(files[i]);
+                }
+            }
+        }
+        return a;
+    }
+}
